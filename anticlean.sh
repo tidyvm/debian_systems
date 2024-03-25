9a6db6a7000a609c7ace655c7696903ff9233d96
Michael Verhulst <michael@terminallabs.com>
2024-03-24 21:56:01 -0500

    Add files via upload

---
+++ anticlean.sh
@@ -0,0 +1,46 @@
+apt remove webapp-manager -y
+apt remove thingy -y
+apt remove hexchat -y
+apt remove drawing -y
+apt remove pix -y
+apt remove pix-dbg -y
+apt remove warpinator -y
+apt remove transmission-gtk -y
+apt remove timeshift -y
+apt remove rhythmbox -y
+apt remove rhythmbox-plugins -y
+apt remove rhythmbox-plugin-tray-icon -y
+apt remove hypnotix -y
+apt remove libreoffice-writer -y
+apt remove libreoffice-math -y
+apt remove libreoffice-writer -y
+apt remove libreoffice-draw -y
+apt remove libreoffice-* -y
+apt remove thunderbird -y
+apt remove thunderbird-* -y
+apt remove shotwell -y
+apt remove webcamoid -y
+apt remove evolution -y
+apt remove gnome-weather -y
+apt remove redshift-gtk -y
+apt remove system-config-printer -y
+apt remove gnome-maps -y
+apt remove mate-utils-common -y
+apt remove atril -y
+
+sudo apt remove gnome-calendar
+sudo apt remove engrampa
+sudo apt remove pluma
+sudo apt remove nemo
+sudo apt remove vim*
+
+
+sudo apt remove nfs-common
+sudo apt remove nfs-*
+
+sudo apt-get purge network-manager
+sudo apt remove libnemo-extension1
+apt purge cinnamon* muffin* nemo* libnemo-extension1
+
+apt clean -y
+apt autopurge -y
