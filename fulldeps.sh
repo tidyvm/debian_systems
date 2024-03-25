d65886cbfe5b2d88be4aeaf4e37a898122521da1
Michael Verhulst <michael@terminallabs.com>
2024-03-24 21:55:33 -0500

    Add files via upload

---
+++ fulldeps.sh
@@ -0,0 +1,366 @@
+## sudo bash bootstrap-salt.sh -M -N git v3005.5
+## curl -o bootstrap-salt.sh -L https://bootstrap.saltproject.io
+
+sudo apt-get update
+sudo apt-get install sudo
+
+## curl -o bootstrap-salt.sh -L https://bootstrap.saltproject.io
+## echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest jammy main" | sudo tee /etc/##apt/sources.list.d/salt.list
+
+## sudo curl -fsSL -o /etc/apt/keyrings/SALT-PROJECT-GPG-PUBKEY-2023.gpg https://repo.saltproject.io/salt/py3/debian/12/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
+## sudo bash ./bootstrap-salt.sh -M -P stable 3005.5
+
+sudo apt install build-essential -y
+sudo apt install linux-headers-$(uname -r) -y
+sudo apt install dkms -y
+
+	apt-get update && apt-get install --assume-yes --no-install-recommends \
+		ca-certificates \
+		dirmngr \
+		dpkg-dev \
+		gcc \
+		gnupg \
+		libbz2-dev \
+		libc6-dev \
+		libexpat1-dev \
+		libffi-dev \
+		liblzma-dev \
+		libsqlite3-dev \
+		libssl-dev \
+		make \
+		netbase \
+		uuid-dev \
+		wget \
+		xz-utils \
+		zlib1g-dev
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+        build-essential \
+        curl \
+        git \
+        libbz2-dev \
+        libncurses5-dev \
+        libncursesw5-dev \
+        libreadline-dev \
+        libsqlite3-dev \
+        libssl-dev \
+        llvm \
+        make \
+        tk-dev \
+        wget \
+        xz-utils \
+        zlib1g-dev
+
+	&& apt-get update && apt-get install -y --no-install-recommends \
+		dpkg-dev \
+		gcc \
+		libbluetooth-dev \
+		libbz2-dev \
+		libc6-dev \
+		libexpat1-dev \
+		libffi-dev \
+		libgdbm-dev \
+		liblzma-dev \
+		libncursesw5-dev \
+		libreadline-dev \
+		libsqlite3-dev \
+		libssl-dev \
+		make \
+		tk-dev \
+		uuid-dev \
+		wget \
+		xz-utils \
+		zlib1g-dev \
+
+sudo apt install gparted -y
+sudo apt install xrdp -y 
+
+sudo apt install ifupdown net-tools -y
+sudo apt install qemu-kvm bridge-utils cpu-checker -y
+sudo apt install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu-kvm -y
+sudo apt install git automake autoconf gcc uml-utilities libtool build-essential git pkg-config -y
+sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
+sudo apt install qemu-kvm qemu-utils uml-utilities bridge-utils socat -y
+sudo apt install build-essential libssl-dev linux-headers-$(uname -r) -y
+sudo apt install virt-manager -y
+sudo apt install virtinst -y
+sudo apt install openvswitch-switch -y
+sudo apt install ovn-host ovn-central -y
+sudo apt install nano curl -y
+sudo apt install uuid -y
+sudo apt install novnc sasl2-bin openvswitch-switch  qemu-kvm vlan -y
+sudo apt install chrony -y
+sudo apt install cloud-image-utils -y
+
+
+sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+
+sudo apt install gcc g++ -y
+
+sudo apt install tmux -y
+
+sudo apt install linux-headers-$(uname -r) -y
+sudo apt install dkms -y
+
+sudo apt install build-essential socat zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+
+sudo apt install python3 python3-dev python3-pip python3-venv python3-lib2to3 python3-gdbm python3-tk -y
+
+sudo apt-get install python3-setuptools -y
+
+sudo apt-get install libcurl4-gnutls-dev -y
+
+sudo apt-get install libgeoip-dev -y
+
+sudo apt-get install cloud-guest-utils -y
+sudo apt-get install cloud-init -y
+sudo apt-get install e2fsprogs -y
+
+sudo apt-get install zsh -y
+
+sudo apt install silversearcher-ag -y
+
+sudo apt-get install netcat-traditional -y
+sudo apt-get install tcpdump -y
+sudo apt-get install rsync -y
+
+sudo apt-get install fdupes watch -y
+
+sudo apt-get install ufw -y
+
+sudo apt install strace -y
+
+sudo apt-get install pv jq -y
+
+sudo apt install whois -y
+sudo apt install expect -y
+
+sudo apt install nano curl -y
+sudo apt install uuid -y
+sudo apt install chrony -y
+sudo apt install tree -y
+sudo apt install nmap -y
+
+sudo apt-get install manpages -y
+sudo apt-get install manpages-dev -y
+
+sudo apt install apt-transport-https lsb-release ca-certificates -y
+ 
+sudo apt install gfortran -y
+
+sudo apt-get install clang -y
+
+sudo apt-get install gzip -y
+sudo apt-get install atop -y
+
+sudo apt-get install glances -y
+sudo apt-get install zip unzip -y
+
+sudo apt-get install libpcre3 libpcre3-dev -y
+
+sudo apt-get install libmaxminddb0 libmaxminddb-dev -y
+sudo apt-get install iptables ebtables dnsmasq-base libxml2-utils iproute2 -y
+sudo apt-get -y install bridge-utils vlan debconf-utils -y
+
+sudo apt-get install openssl libssl-dev -y
+sudo apt-get install -y lxc bridge-utils debootstrap libcap-dev libpam-systemd bridge-utils -y
+
+sudo apt-get install lxc -y
+sudo apt-get install lxcfs -y
+sudo apt-get install libvirt-clients libvirt-daemon-system iptables ebtables dnsmasq-base -y
+sudo apt-get install lxc libvirt0 libpam-cgroup libpam-cgfs bridge-utils cgroupfs-mount -y
+
+sudo apt-get install busybox -y
+sudo apt install lighttpd -y
+
+sudo apt-get install gzip -y
+
+sudo apt install virt-manager libvirt-daemon qemu-kvm libvirt0 virt-manager bridge-utils libvirt-daemon-system qemu-system-x86 qemu-utils dnsmasq -y
+
+sudo apt-get install python3-gi-cairo python3-setuptools python3-cryptography python3-idna python3-openssl python3-socks python3-cryptography python3-idna python3-openssl -y
+
+sudo apt-get install python-is-python3 -y
+
+sudo apt-get install qemu-guest-agent -y
+
+sudo apt-get install bind9 bind9utils bind9-doc -y
+sudo apt-get install bind9 dnsutils -y
+
+sudo apt-get update \
+  && apt-get install -y vim \
+  && apt-get install -y emacs \
+  && apt-get install -y nano \
+  && apt-get install -y curl \
+  && apt-get install -y htop \
+  && apt-get install -y procps \
+  && apt-get install -y findutils -y
+
+sudo apt-get install xz-utils -y
+
+sudo apt-get install bc fakeroot ca-certificates -y
+sudo apt-get install virtinst -y
+sudo apt-get install qemu-kvm virtinst bridge-utils -y
+sudo apt install qemu-kvm qemu-system qemu-utils qemu-user-static -y
+sudo apt install libvirt-clients libvirt-daemon-system virtinst -y
+sudo apt -y install vim libguestfs-tools libosinfo-bin  qemu-system virt-manager -y
+sudo apt-get install qemu-kvm virt-manager virt-viewer  bridge-utils seabios ovmf cpu-checker python3-libvirt  ebtables iptables dnsmasq -y
+sudo apt install apt-transport-https lsb-release ca-certificates -y
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin -y
+sudo apt-get install libvirt-clients libvirt-daemon-system qemu-kvm bridge-utils dnsmasq -y
+
+sudo apt-get install distcc -y
+sudo apt-get install ccache -y
+sudo apt install python3.11 python3.11-pip
+sudo apt-get install gfortran
+sudo apt-get install libpcre3 libpcre3-dev
+sudo apt install -y wget apt-transport-https
+sudo apt install python3-dev
+sudo apt install python3-pip
+sudo apt install python3-venv
+sudo apt install python3-psutil
+sudo apt install python3-dateutil
+sudo apt install python3-setuptools
+sudo apt install python3-poetry
+sudo apt install python3-all
+sudo apt install procps
+sudo apt install binutils binutils-gold
+
+sudo apt install mate-desktop-environment-core
+apt install mate-desktop-environment lightdm
+sudo apt-get install mate-tweak
+sudo apt install mate-utils
+sudo apt install mintmenu -y
+sudo apt install vlc -y
+sudo apt install nmap dnsutils build-essential module-assistant -y
+sudo apt install caja-sendto caja-open-terminal caja-wallpaper caja-xattr-tags caja-image-converter  -y
+sudo apt install ufw dos2unix ark rar unrar sharutils rsync -y
+sudo apt install curl nmap dnsutils -y
+sudo apt install sane sane-utils xsane parallel tesseract-ocr -y
+
+sudo apt install ntfs-3g -y
+sudo apt install ttf-mscorefonts-installer -y
+
+sudo apt install libegl-mesa0 -y
+sudo apt install libegl1-mesa-dev -y
+
+sudo apt install apt-src -y
+sudo apt install apt-move -y
+sudo apt install apt-utils -y
+sudo apt install apt-mirror -y
+sudo apt install libapt-pkg-dev
+sudo apt install libapt-pkg-doc
+
+sudo apt-get install font-manager
+sudo apt install glances
+
+sudo apt install emacs -y
+sudo apt install geany -y
+sudo apt install gimp -y
+sudo apt install imagemagick -y  
+sudo apt install nano -y
+sudo apt install gparted -y
+sudo apt install brasero
+sudo apt install synaptic
+sudo apt install alacarte
+sudo apt install lld llvm
+
+sudo apt install -y libevent-dev
+apt install -y ncurses-dev
+sudo apt install moreutils
+
+
+sudo apt install zip unzip
+sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+sudo apt -y install checkinstall apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -yq install build-essential gdb lcov libbz2-dev libffi-dev libgdbm-dev liblzma-dev libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev lzma lzma-dev tk-dev uuid-dev xvfb zlib1g-dev
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+
+sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
+sudo apt install busybox
+
+sudo apt-get install cmake libc6-dev libc6-dev-i386 g++-multilib -y
+sudo apt-get install libc6-i386 libc6-dev-i386 -y
+sudo apt-get install libc6-dev-i386 -y
+
+sudo apt-get install zip unzip
+sudo apt-get install flatpak
+sudo apt-get install build-dep
+sudo apt-get install bison
+sudo apt-get install make
+sudo apt-get install cmake
+sudo apt-get install build-essential gcc clang libdbus-1-dev libgtk-3-dev libnotify-dev libasound2-dev libcap-dev libcups2-dev libxtst-dev libxss1 libnss3-dev gcc-multilib g++-multilib curl gperf bison
+sudo apt-get install build-essential clang libdbus-1-dev libgtk-3-dev libnotify-dev libgnome-keyring-dev libasound2-dev libcap-dev libcups2-dev libxtst-dev  libxss1 libnss3-dev gcc-multilib g++-multilib curl gperf bison python-dbusmock openjdk-8-jre
+
+sudo apt-get install -y build-essential g++ automake autoconf libbz2-dev libzstd-dev default-libmysqlclient-dev unixodbc-dev libpq-dev libsqlite3-dev libglib2.0-dev libgtk2.0-dev libcurl4-gnutls-dev libgtkglext1-dev libpcre3-dev libsdl-sound1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev libxml2-dev libxslt1-dev librsvg2-dev libpoppler-dev libpoppler-glib-dev libpoppler-private-dev libpoppler-cpp-dev libasound2-dev libdirectfb-dev libxtst-dev libffi-dev libglew-dev libimlib2-dev libv4l-dev libsdl-ttf2.0-dev libgdk-pixbuf2.0-dev linux-libc-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libcairo2-dev libgsl-dev libncurses5-dev libgmime-3.0-dev libalure-dev libgmp-dev libgtk-3-dev libsdl2-dev libsdl2-mixer-dev libsdl2-ttf-dev libsdl2-image-dev sane-utils libdumb1-dev libqt5opengl5-dev libqt5svg5-dev libqt5webkit5-dev libqt5x11extras5-dev qtbase5-dev qtwebengine5-dev libwebkit2gtk-4.0-dev git libssl-dev
+
+sudo apt-get install ntp -y
+apt install firmware-linux* firmware-misc-nonfree acpi-support hwinfo ntpdate hwinfo lshw mc acl \
+            firmware-amd-graphics firmware-realtek firmware-atheros firmware-iwlwifi
+            
+sudo sudo apt install --no-install-recommends git ca-certificates python3 python3-venv libgl1 libxkbcommon-x11-0 libegl1-mesa libfontconfig1 libglib2.0-0 libdbus-1-3 libxcb-cursor0 libxcb-icccm4 libxcb-keysyms1 libxcb-shape0 libnss3 libxcomposite1 libxdamage1 libxrender1 libxrandr2 libxtst6 libxi6 libasound2 -y
+
+sudo apt-get install libfontconfig1-dev libdbus-1-dev libfreetype6-dev libicu-dev libudev-dev libinput-dev libxkbcommon-dev libssl-dev libpng-dev libjpeg-dev libglib2.0-dev
+sudo apt-get -y install build-essential openssl libssl-dev libssl1.0 libgl1-mesa-dev libqt5x11extras5 '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
+sudo apt-get install libssl-dev libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libfontconfig1-dev libxss-dev  libwebp-dev libjsoncpp-dev libopus-dev libminizip-dev libavutil-dev libavformat-dev libavcodec-dev libevent-dev libvpx-dev libsnappy-dev libre2-dev libprotobuf-dev protobuf-compiler -y
+sudo apt-get install bison build-essential gperf flex ruby libasound2-dev libbz2-dev libcap-dev libcups2-dev libdrm-dev libegl1-mesa-dev  libnss3-dev libpci-dev libpulse-dev libudev-dev libxtst-dev gyp ninja-build -y
+
+sudo apt install qtcreator qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools cmake -y
+sudo apt install python3-pyqt5 python3-pyqt5.qtwebengine python3-pyqt5.qtwebchannel libqt5webkit5-dev -y
+sudo apt install python3-pyqt5.qtwebkit libqt5webkit5-dev -y
+
+sudo apt-get install build-essential gcc g++ -y
+sudo apt-get install libxxf86vm-dev libxine2-dev unixodbc-dev libsdl1.2-dev libsdl2-dev -y
+
+sudo apt install git autoconf make texinfo gnutls-bin libgccjit-12-dev gcc libgtk2.0-dev libgnutls28-dev libxpm-dev libgif-dev -y
+
+apt install build-essential wget git python3-pip python3-dev python3-venv \
+     python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev \
+     python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev \
+     libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev -y
+
+sudo apt-get install build-essential gcc g++
+sudo apt-get install libxxf86vm-dev libxine2-dev unixodbc-dev libsdl1.2-dev libsdl2-dev
+sudo apt-get install qtbase5-dev qttools5-dev qtmultimedia5-dev qtdeclarative5-dev libqt5svg5-dev libqt5webkit5-dev libqt5multimedia5-plugins
+sudo apt-get install libgtk2.0-dev libgtk-3-dev libwebkit2gtk-4.0-dev
+sudo apt-get install gtk+-3.0 libgtk-3-dev -f
+sudo apt-get install devhelp -f
+
+sudo apt install qttools5-dev-tools qttools5-dev -y
+sudo apt install qml-module-qtwebkit -y
+
+sudo apt install wkhtmltopdf -y
+
+sudo apt-get install subversion gtk-doc-tools autoconf automake libtool libgtk2.0-dev libpango1.0-dev libicu-dev libxslt-dev libsoup2.4-dev libsqlite3-dev gperf bison flex  autotools-dev libgail-dev -f
+
+sudo apt-get install pulseaudio -y
+sudo apt-get install cmake -y
+sudo apt-get install libvlccore-dev -y
+sudo apt-get install libvlc-dev -y
+sudo apt-get install qttools5-dev-tools -y
+sudo apt-get install qtdeclarative5-dev -y
+sudo apt-get install qtbase5-dev -y
+sudo apt-get install qtmultimedia5-dev -y
+sudo apt-get install qt5-qmake -y
+sudo apt-get install qtmultimedia5-doc -y
+sudo apt-get install qtmultimedia5-doc-html -y
+sudo apt-get install qtmultimedia5-examples -y
+sudo apt-get install qml-module-qtquick-controls -y
+
+sudo apt install python3-gi python3-gi-cairo -y
+
+sudo apt-get install qtcreator qtcreator-data qtcreator-doc qtbase5-examples qtbase5-doc-html qtbase5-dev qtbase5-private-dev cmake -y
+
+sudo apt-get install bison build-essential gperf flex libasound2-dev libcups2-dev libdrm-dev libegl1-mesa-dev libnss3-dev libpci-dev libpulse-dev libudev-dev nodejs libxtst-dev gyp ninja-build -y
+sudo apt-get install libegl-dev libgles-dev libglu1-mesa-dev libqt5positioning5 libqt5sensors5 libqt5webchannel5 libvulkan-dev libxext-dev qdoc-qt5 qhelpgenerator-qt5 qt5-qmake qt5-qmake-bin qtattributionsscanner-qt5 x11proto-xext-dev -y
+
+
+sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt-get install certbot ntpsec-doc ntpsec-ntpviz -y
