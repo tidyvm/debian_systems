14bb1a77734a87b92a7d3b9343bd58f0aa8bc80e
Michael Verhulst <michael@terminallabs.com>
2024-03-24 21:56:48 -0500

    Add files via upload

---
+++ suggested.sh
@@ -0,0 +1,56 @@
+  binutils-doc cmake-doc cmake-format elpa-cmake-mode ninja-build cpp-doc
+  gcc-12-locales cpp-12-doc dbus-user-session pinentry-gnome3 tor
+  debian-keyring g++-multilib g++-12-multilib gcc-12-doc gcc-multilib autoconf
+  automake libtool flex bison gdb gcc-doc gcc-12-multilib git-daemon-run
+  | git-daemon-sysvinit git-doc git-email git-gui gitk gitweb git-cvs
+  git-mediawiki git-svn parcimonie xloadimage scdaemon lrzip glibc-doc
+  libcurl4-doc libidn-dev libkrb5-dev libldap2-dev librtmp-dev libssh2-1-dev
+  zlib1g-dev bzr libgd-tools icu-doc postgresql-doc-15 libssl-doc
+  libstdc++-12-doc make-doc ed diffutils-doc pinentry-doc python3-braceexpand
+
+  binutils-doc cpp-doc gcc-12-locales cpp-12-doc dbus-user-session
+  pinentry-gnome3 tor debian-keyring g++-multilib g++-12-multilib gcc-12-doc
+  gcc-multilib autoconf automake libtool flex bison gdb gcc-doc
+  gcc-12-multilib parcimonie xloadimage scdaemon glibc-doc git bzr libgd-tools
+  libstdc++-12-doc make-doc ed diffutils-doc pinentry-doc
+
+
+doxygen-latex doxygen-doc doxygen-gui fontforge fonts-roboto libavalon-framework-java
+  libcommons-logging-java-doc libexcalibur-logkit-java liblog4j1.2-java libcurl4-doc libidn-dev
+  libkrb5-dev librtmp-dev libssh2-1-dev openjdk-17-demo openjdk-17-source visualvm perl-tk xzdec
+  texlive-fonts-extra-doc texlive-fonts-recommended-doc texlive-latex-base-doc wp2latex
+  icc-profiles texlive-latex-extra-doc texlive-science texlive-latex-recommended-doc
+  texlive-luatex texlive-pstricks dot2tex prerex texlive-pictures-doc vprerex tipa-doc
+
+
+build-essential
+cmake
+ninja-build
+gfortran
+pkg-config
+python-dev
+libopenblas-dev
+liblapack-dev
+cython3
+patchelf
+libatlas-base-dev
+libffi-dev
+
+  cmake \
+  make \
+  libpq-dev \
+  libcurl4  \
+  libcurl4-openssl-dev  \
+  libssl-dev  \
+  libxml2 \
+  libxml2-dev  \
+  pkg-config \
+  ca-certificates \
+  xclip
+
+  git \
+  wget \
+  zip \
+  unzip \
+  rsync \
+  bash-completion \
